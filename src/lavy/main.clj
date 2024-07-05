149c01100aa46c4bf34dbade0750c2b5667bf96b
gmrowe <garrett.m.rowe@gmail.com>
2024-07-04 19:10:26 -0700

    Implement count bytes functionality

--- src/lavy/main.clj
+++ src/lavy/main.clj
@@ -1,3 +1,35 @@
-(ns lavy.main)
+(ns lavy.main
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
 
-(defn run [_opts] (println "Hello World!"))
+(defn exec-command
+  [command rdr]
+  (when (= command :count-bytes)
+    (-> (.getBytes (slurp rdr))
+        seq
+        count)))
+
+(defn parse-arg
+  [arg]
+  ({"-c" :count-bytes} arg))
+
+(defn run
+  [command-arg file-path]
+  (with-open [rdr (io/reader file-path)]
+    (let [result (exec-command (parse-arg command-arg) rdr)]
+      (printf "%d %s%n" result file-path))))
+
+(def usage
+  (str/join
+   \newline
+   ["Usage: lavy [-c] filename"
+    "    -c: Count the number of bytes in the supplied file"
+    ""]))
+
+(defn -main
+  [& args]
+  (if (< (count args) 2)
+    (print usage)
+    (run (first args) (second args))))
+
--- test/lavy/main_test.clj
+++ test/lavy/main_test.clj
@@ -1,5 +1,20 @@
 (ns lavy.main-test
-  (:require [clojure.test :refer [testing is deftest]]))
+  (:require [clojure.test :refer [testing is deftest]]
+            [clojure.java.io :as io]
+            [lavy.main :as m]))
 
-(deftest test-my-tests
-  (testing "Neil DeGrasse Tyson is right" (is (= 1 (* 1 1)))))
+(deftest parse-args-test
+  (testing "Arg `-c` corresponds to :count-bytes"
+    (is (= :count-bytes (m/parse-arg "-c")))))
+
+(deftest main-test
+  (testing "Calling -main with no args results in usage string"
+    (is (= m/usage (with-out-str (m/-main)))))
+  (testing "Calling -main with 1 arg results in usage string"
+    (is (= m/usage (with-out-str (m/-main ["-c"]))))))
+
+(deftest exec-command-test
+  (testing "Calling (run :count-bytes rdr) returns bytes in rdr"
+    (is (= 12
+           (m/exec-command :count-bytes
+                           (-> "Hello World!" char-array io/reader))))))
