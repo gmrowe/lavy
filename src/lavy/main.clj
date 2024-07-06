e6dcddf70c856df61168158458c08003edd522f4
Garrett Rowe <garrett.m.rowe@gmail.com>
2024-07-06 16:27:45 -0700

    Implement correct output for multiple commands from a single file

--- src/lavy/main.clj
+++ src/lavy/main.clj
@@ -4,14 +4,46 @@
 
 (defn exec-command
   [command rdr]
-  (cond (= command :count-bytes) (let [text (slurp rdr)]
-                                   (count (seq (.getBytes text))))
-        (= command :count-lines) (let [text (slurp rdr)]
-                                   (count (re-seq #"\r?\n" text)))
-        (= command :count-words) (let [text (slurp rdr)]
-                                   (count (str/split text #"\s+")))
-        (= command :count-chars) (let [text (slurp rdr)]
-                                   (.count (.codePoints text)))))
+  (let [text (slurp rdr)]
+    (case command
+      :count-bytes (count (seq (.getBytes text)))
+      :count-lines (count (re-seq #"\r?\n" text))
+      :count-words (count (str/split text #"\s+"))
+      :count-chars (.count (.codePoints text)))))
+
+(defn format-output
+  [outputs]
+  (let [output (first outputs)
+        fmt (fn [opt] (if opt (format "%8d" opt) ""))]
+    (format "%s%s%s%s %s"
+            (fmt (:count-lines output))
+            (fmt (:count-words output))
+            (fmt (:count-chars output))
+            (fmt (:count-bytes output))
+            (:file-path output))))
+
+(def opt-map
+  {\c :count-bytes, \l :count-lines, \m :count-chars, \w :count-words})
+
+(defn parse-opt-sequence [opts] (mapv opt-map (next opts)))
+
+(defn parse-args
+  [args]
+  (let [[opts filenames] (partition-by #(str/starts-with? % "-") args)]
+    {:options (mapcat parse-opt-sequence opts), :files filenames}))
+
+(defn exec-all
+  [{:keys [options files]}]
+  (for [opt options
+        file files]
+    {opt (exec-command opt (io/reader file)), :file-path file}))
+
+(defn exec-commands
+  [opt-map]
+  (map #(apply merge %)
+    (->> (exec-all opt-map)
+         (group-by :filename)
+         vals)))
 
 (defn parse-arg
   [arg]
@@ -19,19 +51,19 @@
    arg))
 
 (defn run
-  [command-arg file-path]
-  (with-open [rdr (io/reader file-path)]
-    (let [result (exec-command (parse-arg command-arg) rdr)]
-      (printf "%d %s%n" result file-path))))
+  [args]
+  (let [[command-arg file-path] args]
+    (with-open [rdr (io/reader file-path)]
+      (let [result (exec-command (parse-arg command-arg) rdr)]
+        (printf "%d %s%n" result file-path)))))
 
 (def usage
   (str/join \newline
-            ["Usage: lavy [-c] filename"
+            ["Usage: lavy [-clmw] [file]"
              "    -c: Count the number of bytes in the supplied file"
              "    -l: Count the number of newlines in the supplied file"
-             "    -w: Count the number of words in the supplied file" ""]))
+             "    -w: Count the number of words in the supplied file"
+             "    -m: Count the number of characters in the supplied file" ""]))
 
-(defn -main
-  [& args]
-  (if (< (count args) 2) (print usage) (run (first args) (second args))))
+(defn -main [& args] (if (< (count args) 2) (print usage) (run args)))
 
--- test/lavy/main_test.clj
+++ test/lavy/main_test.clj
@@ -1,6 +1,7 @@
 (ns lavy.main-test
   (:require [clojure.test :refer [testing is deftest]]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [lavy.main :as m]))
 
 (defn rdr-from-str
@@ -9,7 +10,7 @@
       char-array
       io/reader))
 
-(deftest parse-args-test
+(deftest parse-arg-test
   (testing "Arg `-c` corresponds to :count-bytes"
     (is (= :count-bytes (m/parse-arg "-c"))))
   (testing "Arg `-l` corresponds to :count-lines"
@@ -18,6 +19,30 @@
     (is (= :count-words (m/parse-arg "-w"))))
   (testing "Arg `-m` corresponds to :count-chars"
     (is (= :count-chars (m/parse-arg "-m")))))
+
+(deftest parse-args-test
+  (testing "Arg -c followed by single filename"
+    (is (= {:options [:count-bytes], :files ["file.name"]}
+           (m/parse-args ["-c" "file.name"]))))
+  (testing "Arg -l followed by a single filename"
+    (is (= {:options [:count-lines], :files ["file.txt"]}
+           (m/parse-args ["-l" "file.txt"]))))
+  (testing "Arg -w followed by a single filename"
+    (is (= {:options [:count-words], :files ["file.dat"]}
+           (m/parse-args ["-w" "file.dat"]))))
+  (testing "Arg -m followed by a single filename"
+    (is (= {:options [:count-chars], :files ["file.txt"]}
+           (m/parse-args ["-m" "file.txt"]))))
+  (testing "Multiple opts with single filename"
+    (is (= {:options [:count-bytes :count-words], :files ["file.csv"]}
+           (m/parse-args ["-c" "-w" "file.csv"]))))
+  (testing "Multiple short form opts single filename"
+    (is (= {:options [:count-lines :count-words], :files ["file.xls"]}
+           (m/parse-args ["-lw" "file.xls"]))))
+  (testing "Opts with multiple filenames"
+    (is (= {:options [:count-lines :count-words],
+            :files ["file-1.txt" "file-2.txt"]}
+           (m/parse-args ["-lw" "file-1.txt" "file-2.txt"])))))
 
 (deftest main-test
   (testing "Calling -main with no args results in usage string"
@@ -38,3 +63,31 @@
     (is (= 12 (m/exec-command :count-chars (rdr-from-str "Hello World!")))))
   (testing "Counting chars of an UTF-8 string"
     (is (= 12 (m/exec-command :count-chars (rdr-from-str "Ḩ℮ɭιȭ 𝖂ọ𝘳ȴ𝖉!"))))))
+
+(deftest output-results-test
+  (testing "Results from single commnd single filename"
+    (is (= "  174355 alice.txt"
+           (m/format-output [{:file-path "alice.txt", :count-chars 174355}]))))
+  (testing "Results from multiple commands single filename"
+    (is (= "    3756   29564  174355 alice.txt"
+           (m/format-output [{:file-path "alice.txt",
+                              :count-bytes 174355,
+                              :count-words 29564,
+                              :count-lines 3756}]))))
+  #_(testing "Results from multiple commands multiple files"
+      (is (= (str/join \newline
+                       ["    3756   29564  174355 alice.txt"
+                        "    7145   58164  342190 test.txt"
+                        "   10901   87728  516545 total"])
+             (m/format-output [{:file-path "alice.txt",
+                                :count-bytes 174355,
+                                :count-words 29564,
+                                :count-lines 3756}
+                               {:file-path "test.txt",
+                                :count-bytes 342190,
+                                :count-words 58164,
+                                :count-lines 7145}])))))
+
+
+(comment
+  "The order of output always takes the form of line, word, byte, and file name.")
