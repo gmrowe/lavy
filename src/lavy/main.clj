7e414a1220b3b02d04cb9485526f0e6ee76a9f9e
Garrett Rowe <garrett.m.rowe@gmail.com>
2024-07-05 19:00:53 -0700

    Support the -w and -m flags

--- src/lavy/main.clj
+++ src/lavy/main.clj
@@ -1,37 +1,36 @@
 (ns lavy.main
   (:require [clojure.java.io :as io]
-            [clojure.pprint :as pp]
             [clojure.string :as str]))
 
 (defn exec-command
   [command rdr]
-  (cond
-    (= command :count-bytes)(-> (.getBytes (slurp rdr)) seq count)
-    (= command :count-lines) (->> (.getBytes (slurp rdr))
-                                  seq
-                                  (filter #(= (byte \newline) %))
-                                  count)))
+  (cond (= command :count-bytes) (let [text (slurp rdr)]
+                                   (count (seq (.getBytes text))))
+        (= command :count-lines) (let [text (slurp rdr)]
+                                   (count (re-seq #"\r?\n" text)))
+        (= command :count-words) (let [text (slurp rdr)]
+                                   (count (str/split text #"\s+")))
+        (= command :count-chars) (let [text (slurp rdr)] (count text))))
+
 (defn parse-arg
   [arg]
-  ({"-c" :count-bytes
-    "-l" :count-lines} arg))
+  ({"-c" :count-bytes, "-l" :count-lines, "-w" :count-words, "-m" :count-chars}
+   arg))
 
 (defn run
   [command-arg file-path]
-  (with-open [rdr (io/input-stream file-path)]
+  (with-open [rdr (io/reader file-path)]
     (let [result (exec-command (parse-arg command-arg) rdr)]
       (printf "%d %s%n" result file-path))))
 
 (def usage
-  (str/join
-   \newline
-   ["Usage: lavy [-c] filename"
-    "    -c: Count the number of bytes in the supplied file"
-    ""]))
+  (str/join \newline
+            ["Usage: lavy [-c] filename"
+             "    -c: Count the number of bytes in the supplied file"
+             "    -l: Count the number of newlines in the supplied file"
+             "    -w: Count the number of words in the supplied file" ""]))
 
 (defn -main
   [& args]
-  (if (< (count args) 2)
-    (print usage)
-    (run (first args) (second args))))
+  (if (< (count args) 2) (print usage) (run (first args) (second args))))
 
--- test/lavy/main_test.clj
+++ test/lavy/main_test.clj
@@ -5,13 +5,19 @@
 
 (defn rdr-from-str
   [s]
-  (-> s char-array io/reader))
+  (-> s
+      char-array
+      io/reader))
 
 (deftest parse-args-test
   (testing "Arg `-c` corresponds to :count-bytes"
     (is (= :count-bytes (m/parse-arg "-c"))))
   (testing "Arg `-l` corresponds to :count-lines"
-    (is (= :count-lines (m/parse-arg "-l")))))
+    (is (= :count-lines (m/parse-arg "-l"))))
+  (testing "Arg `-w` corresponds to :count-words"
+    (is (= :count-words (m/parse-arg "-w"))))
+  (testing "Arg `-m` corresponds to :count-chars"
+    (is (= :count-chars (m/parse-arg "-m")))))
 
 (deftest main-test
   (testing "Calling -main with no args results in usage string"
@@ -21,9 +27,12 @@
 
 (deftest exec-command-test
   (testing "Calling (exec-command :count-bytes rdr) returns bytes in rdr"
-    (is (= 12
-           (m/exec-command :count-bytes
-                           (rdr-from-str "Hello World!")))))
+    (is (= 31 (m/exec-command :count-bytes (rdr-from-str "Ḩ℮ɭιȭ 𝖂ọ𝘳ȴ𝖉!")))))
   (testing "Calling (exec-command :count-lines rdr) returns num lines in rdr"
-    (is (= 2
-           (m/exec-command :count-lines (rdr-from-str "abc\ndef\ng h i"))))))
+    (is (= 2 (m/exec-command :count-lines (rdr-from-str "abc\ndef\ng h i")))))
+  (testing "Calling (exec-command :count-words rdr) retruns words in rdr"
+    (is (= 5
+           (m/exec-command :count-words
+                           (rdr-from-str "car star\nwar is bad")))))
+  (testing "Calling (exec-command :count-chars rdr) returns chars in rdr"
+    (is (= 12 (m/exec-command :count-chars (rdr-from-str "Hello World!"))))))
